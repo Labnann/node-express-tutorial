35e3d78b20413d2b54c86dda25b325f7afdf9978
Labnan <khalidmasum@iut-dhaka.edu>
2021-06-03 23:44:38 +0600

    Setting up basic local host http

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const http = require('http')
 
 const server = http.createServer(((req, res) => {
-    console.log(req);
-    res.write("welcome");
-    res.end();
+    if(req.url==='/'){
+        res.end('Welcome to our homepage');
+    }
+    if(req.url === '/about'){
+        res.end('Here is our short history')
+    }
+
+    res.end("Oops!");
+
 
 }))
 
