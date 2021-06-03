50d0b4eb100ad2bc7daada6b5ba4fa328fb66490
Labnan <khalidmasum@iut-dhaka.edu>
2021-06-03 23:05:24 +0600

    Read write async dirty solution

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-const {readFileSync, writeFileSync} = require('fs');
-const first = readFileSync('./content/first.txt','utf-8')
-const second = readFileSync('./content/second.txt','utf-8')
+const {readFile, writeFile} = require('fs');
 
-console.log(first,second);
-
-
-writeFileSync('./content/result-sync.txt',
-    `Here is the result: ${first}, ${second}`, {flag: 'a'}
-)
+readFile('./content/first.txt','utf-8',(err,result)=>{
+    if(err) {
+        console.log(err);
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt','utf-8', (err,result)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const second = result;
+        writeFile(
+            './content/result-async.txt',
+            `here is the result: ${first}, ${second}`,
+            (err,result)=>{
+                if(err) console.log(err);
+                console.log(result);
+            }
+        )
+    } )
+} )
